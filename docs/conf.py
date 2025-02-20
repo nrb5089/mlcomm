54e5a39e3a38415bfb0b21d3b9ac34d6d66dcb5a
Nate Blinn <nblinn6@gatech.edu>
2025-02-20 11:21:58 -0800

    Update docs 20FEB2025

--- docs/conf.py
+++ docs/conf.py
@@ -37,8 +37,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_css_files = ['custom.css']
 #html_logo = 'media/gtlogo.png'
 
 autodoc_mock_imports = ['numpy', 'scipy', 'matplotlib']
